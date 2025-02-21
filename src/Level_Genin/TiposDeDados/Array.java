2300897b0224d4dfcf78e302924199c661b3f4f0
bequineper <quineperequineper@gmail.com>
2025-02-21 00:16:21 -0300

    ESSA BOSTA RODOU DE PRIMEIRA KKKKKKKKKKK VSF ACHEI QUE IA QUEBRAR, E NO FIM DEU CERTINHO PERFEITO COMO EU IMAGINEI

--- src/Level_Genin/TiposDeDados/Array.java
+++ src/Level_Genin/TiposDeDados/Array.java
@@ -3,37 +3,155 @@ package Level_Genin.TiposDeDados;
 public class Array {
     public static void main(String[] args) {
 
-        /*
+    String[][] ninjasAndVillages = new String[3][3];
+
+    ninjasAndVillages [0] [0] = "Konoha";
+    ninjasAndVillages [0] [1] = "Naruto";
+    ninjasAndVillages [0] [2] = "Sasuke";
+
+    ninjasAndVillages [1] [0] = "Nevoa";
+    ninjasAndVillages [1] [1] = "Zabuza";
+    ninjasAndVillages [1] [2] = "Haku";
+
+    ninjasAndVillages [2] [0] = "Desert";
+    ninjasAndVillages [2] [1] = "Gaara";
+    ninjasAndVillages [2] [2] = "Temari";
+
+        for (int i = 0; i < ninjasAndVillages.length ; i++) {
+        System.out.println("Village: " + ninjasAndVillages[i][0] + " - Ninja 1: " +
+                ninjasAndVillages[i][1] + " - Ninja 2: " + ninjasAndVillages[i][2]);
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*   *//*
         String ninja1 = "Naruto";
         String ninja2 = "Sasuke";
         String ninja3 = "Sakura";
-        */
+        *//*
 
 //        int[] ages = new int[3];
 
 //        Initiate an array
 
         String[] ninja = new String[5];
+
         ninja[0] = "Naruto";
         ninja[1] = "Sasuke";
         ninja[2] = "Sakura";
         ninja[3] = "Hinata";
         ninja[4] = "Kakashi";
 
-        ninja = new String[7];
-        ninja[0] = "Hashirama";
+
+        // Redefine the variable ninja
+        // Arrays type string initiate like "null"
+        *//*ninja[0] = "Hashirama";
         ninja[1] = "Tobirama";
         ninja[2] = "Hiruzen";
         ninja[3] = "Minato";
         ninja[4] = "Tsunade";
         ninja[5] = "Kakashi";
-        ninja[6] = "Naruto";
-        System.out.println(ninja[3] );
+        ninja[6] = "Naruto";*//*
+//        System.out.println(ninja[3] );
+
+        String nameninja = "Naruto";
+        nameninja = "Boruto";
+        System.out.println(nameninja);
+
+
+
+
+
+
+
+
+        // Arrays type inter/short/byte initiate like "0"
+        // Array to the age
+        *//*short[] age = new short[2];
+        age[0] = 16;
+        System.out.println(age[0]);
+
+        // Arrays type boolean initiate like false"
+        boolean[] trueorfalse = new boolean[1];
+        System.out.println(trueorfalse[0]);
+
+        // Arrays type double initiate like "0.0"
+        double[] floating = new double[1];
+        System.out.println(floating[0]);*//*
+
+
+
 
         // For to make a loop in the array
-        for (int i = 0; i < 7 ; i++) {
+        *//*for (int i = 0; i < 7 ; i++) {
             System.out.println(ninja[i]);
-        }
+        }*/
 
 
 
